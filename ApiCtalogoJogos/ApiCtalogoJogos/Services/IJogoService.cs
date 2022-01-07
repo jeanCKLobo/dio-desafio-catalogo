df0ef83006546ebd682d5eafc9057b7fcbc5558b
jeanCKLobo <jean-kaneko@hotmail.com>
2022-01-07 16:48:01 -0300

    catalogo

---
+++ ApiCtalogoJogos/ApiCtalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,20 @@
+﻿using ApiCtalogoJogos.ViewModel;
+using ApiCtalogoJogos.InputModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCtalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid guid);
+        Task<JogoViewModel> Inserir(JogoInputmodel jogo);
+        Task Atualizar(Guid id, JogoInputmodel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid guid);
+
+    }
+}
